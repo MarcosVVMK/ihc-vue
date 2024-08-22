3bcc8afc9912c98071eb37192da28a2327faf001
Marcos Matos <marcos@coffee-code.tech>
2024-08-21 23:26:17 -0300

    feat(logo): finish logo

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,23 +1,10 @@
-<template>
-  <header class="bg-sky-300 dark:bg-gray-900 p-16 header-container">
-    <div class="flex flex-col items-center mb-4">
-      <h1 class="text-black dark:text-white text-2xl font-bold mb-2">SIGH - Sistema de Gerenciamento Hospitalar</h1>
-    </div>
-    <div class="breadcrumbs-container">
-      <Breadcrumbs :currentStep="currentStep" @update-step="$emit('update-step', $event)" />
-    </div>
-    <div class="darkmode-toggle-container">
-      <DarkModeToggle />
-    </div>
-  </header>
-</template>
-
 <script>
 import Breadcrumbs from "./Breadcrumbs.vue";
 import DarkModeToggle from "./DarkModeToggle.vue";
+import Logo from "./Logo.vue";
 
 export default {
-  components: { Breadcrumbs, DarkModeToggle },
+  components: {Logo, Breadcrumbs, DarkModeToggle },
   props: {
     currentStep: {
       type: Number,
@@ -27,7 +14,22 @@ export default {
   }
 };
 </script>
-
+<template>
+  <header class="bg-sky-300 dark:bg-gray-900 p-16 header-container">
+    <div class="logo-container">
+      <Logo :currentStep="currentStep" @update-step="$emit('update-step', $event)"  />
+    </div>
+    <div class="flex flex-col items-center mb-4">
+      <h1 class="text-black dark:text-white text-2xl font-bold mb-2">Sistema de Gerenciamento Hospitalar</h1>
+    </div>
+    <div class="breadcrumbs-container">
+      <Breadcrumbs :currentStep="currentStep" @update-step="$emit('update-step', $event)" />
+    </div>
+    <div class="darkmode-toggle-container">
+      <DarkModeToggle />
+    </div>
+  </header>
+</template>
 <style scoped>
 .breadcrumbs-container {
   max-width: 28rem;
@@ -46,5 +48,10 @@ export default {
   position: absolute;
   top: 1rem;
   right: 1rem;
+}
+.logo-container{
+  position: absolute;
+  top: 1rem;
+  left: 4rem;
 }
 </style>
---
+++ src/components/partials/Logo.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineEmits } from 'vue';
+
+const emit = defineEmits(['update-step']);
+
+function updateStep() {
+  emit('update-step', 1);
+}
+</script>
+
+<template>
+  <div class="flex items-center cursor-pointer" @click="updateStep">
+    <img src="../../assets/images/logo.png" alt="Logo" class="w-12 h-12" />
+    <span class="text-black dark:text-white text-2xl font-bold ml-2">SIGH</span>
+  </div>
+</template>
+
+<style scoped>
+</style>
